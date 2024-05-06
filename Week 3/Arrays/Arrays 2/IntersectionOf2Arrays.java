f759be9c2e51adb48e023161c581a480849ba8b9
aryangupta1105 <husltegamingfanclub@gmail.com>
2024-05-06 23:55:23 +0530

    Correction in intersection question

--- Week 3/Arrays/Arrays 2/IntersectionOf2Arrays.java
+++ Week 3/Arrays/Arrays 2/IntersectionOf2Arrays.java
@@ -3,16 +3,22 @@ import java.util.Vector;
 public class IntersectionOf2Arrays {
     static void intersectionOf2Arrays(Vector<Integer> v1 , Vector<Integer> v2){
         Vector<Integer> ans = new Vector<>();
+        int flag = 0; 
         for(int i = 0; i<v1.size(); i++){
             for(int j = 0; j<v2.size(); j++)
             {
                 if(v1.get(i) == v2.get(j)) {
                     v2.set(j , -1);
-                    ans.add(v1.get(i));
+                    flag = 1;
+                    break;
+                }
+                else{
+                    flag = 0;
                 }
             }
+            if(flag == 1) ans.add(v1.get(i));
         }
-        
+      
         System.out.println(ans);
     }
     public static void main(String args[]){
