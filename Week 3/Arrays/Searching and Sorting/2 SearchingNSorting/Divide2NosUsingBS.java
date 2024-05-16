17b8c481168fb13994c53e12560b6964e4a9c33e
aryangupta1105 <husltegamingfanclub@gmail.com>
2024-05-16 23:12:39 +0530

    searching and Sorting class 3

--- Week 3/Arrays/Searching and Sorting/2 SearchingNSorting/Divide2NosUsingBS.java
+++ Week 3/Arrays/Searching and Sorting/2 SearchingNSorting/Divide2NosUsingBS.java
@@ -4,31 +4,41 @@ public class Divide2NosUsingBS {
     static int divide2NosUsingBS(int n1 , int n2){
         Scanner sc = new Scanner(System.in);
         int start = 0; 
-        int end = n1;
+        int end = Math.abs(n1);
         int ans = 0;
         int mid = 0;
         while(start<=end){
             
             mid = start + (end-start)/2;
-            if(n1/n2 == mid) ans = mid;
+            if(Math.abs(n1)== Math.abs(mid*n2)) {
+                ans = mid;
+                break;
+            }
             
-            if (n1/n2 < mid) {
+            if (Math.abs(n1) < Math.abs(mid*n2)) {
                 end = mid-1;
             }
-            else start = mid+1;
+            else{ 
+                ans = mid;
+                start = mid+1;
+            }
             
         }
-        
-        return mid;
+       if((n1>0 && n2>0) || (n1<0 && n2<0))
+            return ans;
+       else 
+            return -ans;
+       
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the numbers: ");
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        int quotient = divide2NosUsingBS(n1, n2);
+        int divident = sc.nextInt();
+        int divisor = sc.nextInt();
+        int quotient = divide2NosUsingBS(divident, divisor);
         System.out.println(quotient);
-        System.out.println("Enter the precision:");
+        
+       
         
     }
 }
---
+++ Week 3/Arrays/Searching and Sorting/2 SearchingNSorting/FindOddOccuringElement.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class FindOddOccuringElement {
+    // using xor method
+    static void findOddOccuringElement(int arr[]){
+        int ans = 0;
+        for(int i = 0; i<arr.length; i++){
+            ans = ans^arr[i];
+        }
+        System.out.println(ans);
+    }
+    static int findOddOccuringElementBS(int arr[]){
+        int s = 0;
+        int e= arr.length-1;
+        int mid = 0;
+        int ans = 0;
+        while(s<e){
+            mid = s + (e-s)/2;
+            if(mid%2 == 0 )
+            {
+                if(arr[mid] == arr[mid+1])
+                s= mid+2;
+                else if(arr[mid] == arr[mid-1])
+                e = mid-2;
+                else{
+                    return mid;
+                }
+            }  
+            else{
+                if(arr[mid] == arr[mid-1])
+                s= mid+2;
+                else if(arr[mid] == arr[mid-1])
+                e = mid-2;
+                else{
+                    return mid;
+                }
+            }
+        }
+        return mid;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5,4};
+        // findOddOccuringElement(arr);
+        int ans = findOddOccuringElementBS(arr);
+        System.out.println(ans);
+
+    }
+}
